25235d5f3e2d7b819a420f45a824c331ff7b42ec
Khrystyna Sen <anollina@ukr.net>
2025-05-19 16:43:57 +0300

    Added urls to app

---
+++ payments/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from payments.views import PaymentViewSet
+
+router = DefaultRouter()
+router.register(r"", PaymentViewSet, basename="payments")
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
+
+app_name = "payments"
