cb15e8a3f62b7ec855892cdfe591496ccdf36dc3
AndyStar <andy_star@ukr.net>
2025-05-19 16:42:44 +0300

    Model borrowings added. Double quotes flake8 message fixed

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from rest_framework.exceptions import ValidationError
 
+from books.models import Book
 from config import settings
 
 
@@ -10,13 +12,28 @@ class Borrowing(models.Model):
         on_delete=models.CASCADE,
         related_name="borrowings",
     )
-    book = models.ForeignKey("Book", on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
     borrow_date = models.DateField(null=False, blank=False)
     expected_return_date = models.DateField(null=False, blank=False)
     actual_return_date = models.DateField(null=True, blank=True)
 
+    def clean(self: "Borrowing") -> None:
+        if self.expected_return_date <= self.borrow_date:
+            raise ValidationError({
+                "expected_return_date": "Expected return date must "
+                "be greater than borrow date."
+            })
+        if (
+            self.actual_return_date is not None
+            and self.actual_return_date <= self.borrow_date
+        ):
+            raise ValidationError({
+                "actual_return_date": "If actual return date exists it "
+                                      "must be greater than borrow date."
+            })
+
     @property
-    def is_active(self):
+    def is_active(self: "Borrowing") -> bool:
         return self.actual_return_date is None
 
     class Meta:
