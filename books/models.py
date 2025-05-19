49c31b08774767ae8a3fccf9568c5341eb0357ae
Roksolana Kalinska <woops359@gmail.com>
2025-05-19 10:55:32 +0300

    Edit the path of images

--- books/models.py
+++ books/models.py
@@ -9,7 +9,7 @@ def book_image_file_path(instance, filename):
     _, extension = os.path.splitext(filename)
     filename = f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
 
-    return os.path.join("uploads/books/", filename)
+    return os.path.join("books/", filename)
 
 
 class Book(models.Model):
