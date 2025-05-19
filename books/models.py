d472c332a5337a1af03441d290a5c3b648394d51
Roksolana Kalinska <woops359@gmail.com>
2025-05-19 12:06:17 +0300

    Edit: Added choice field to cover

--- books/models.py
+++ books/models.py
@@ -1,5 +1,6 @@
 import os
 import uuid
+from enum import unique
 
 from django.db import models
 from django.utils.text import slugify
@@ -12,10 +13,15 @@ def book_image_file_path(instance: "Book", filename: str) -> str:
 
     return os.path.join("books/", filename)
 
+@unique
+class BookCoverType(models.TextChoices):
+    HARD = "Hard", "Hardcover"
+    SOFT = "Soft", "Softcover"
+
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
-    cover = models.CharField(max_length=4)
+    cover = models.CharField(max_length=4, choices=BookCoverType.choices, default=BookCoverType.HARD)
     inventory = models.IntegerField()
     daily_fee = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(null=True, blank=True, upload_to=book_image_file_path)
