f0851cd408c91173d22727cb16ad8c1a9de2462b
Sotnikov Oleksandr <ssotnikovvv55@gmail.com>
2025-05-19 12:15:19 +0300

    Created views and routers

---
+++ authors/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from authors.views import AuthorViewSet
+
+
+router = DefaultRouter()
+router.register(r"authors", AuthorViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
+
+app_name = "authors"
