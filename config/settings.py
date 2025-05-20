cad9b3c02e976deed36346700c11e70c00c86419
Oneyura <hryn.yuri@gmail.com>
2025-05-20 14:17:13 +0300

    Update installed apps in settings
    add rest_framework_simplejwt.token_blacklist

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # Third-party apps
     "rest_framework",
+    "rest_framework_simplejwt.token_blacklist",
     "debug_toolbar",
     # local apps
     "books",
