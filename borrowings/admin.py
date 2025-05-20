bee1837993b554c1735dfa0b388ca19c3acdbe81
Roksolana Kalinska <woops359@gmail.com>
2025-05-20 11:16:06 +0300

    Changed design of borrowings admin panel

--- borrowings/admin.py
+++ borrowings/admin.py
@@ -4,7 +4,7 @@ from borrowings.models import Borrowing
 
 
 @admin.register(Borrowing)
-class AuthorAdmin(admin.ModelAdmin):
+class Borrowing(admin.ModelAdmin):
     list_display = ("user", "book", "created_at", "borrow_date", "expected_return_date", "actual_return_date")
     search_fields = ("user", "book", "borrow_date", "expected_return_date")
     list_filter = ("user","book", "borrow_date", "expected_return_date")
